893205d08bd2121052dc138e266323d2aa5d734a
AngelusQQ <william.ma9215@gmail.com>
2025-03-14 19:10:35 -0400

    Replaced Firebase Client SDK with Firebase Admin SDK Boilerplate

--- database/firebaseConfig.js
+++ database/firebaseConfig.js
@@ -1,29 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const firebase = require('firebase/app');
-const { getDatabase } = require("firebase/database");
-const { getAnalytics } = require("firebase/analytics");
+const admin = require("firebase-admin");
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+const serviceAccount = require('./' + process.env.SERVICE_ACCOUNT_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+});
 
 // Export the database reference
 module.exports = {
-  analytics: getAnalytics(app),
-  database: getDatabase(app)
-};
-
+  database: admin.database()
+}
