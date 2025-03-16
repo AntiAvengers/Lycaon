028662a8fc0caebf1a780a2cf749593c4c3d8ac8
Eva Li <evali3101@gmail.com>
2025-03-16 18:28:24 -0400

    added components folds with different pages, added react router dom and added routes in app.js

---
+++ client/src/components/signInPg.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const SignInPg = () => {
+    return (
+        <div>
+            <h1>SignIn</h1>
+        </div>
+    )
+};
+
+export default SignInPg;
