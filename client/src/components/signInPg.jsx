66e4c51273bc16d046b1c994a79d2110c4c360c4
Eva Li <evali3101@gmail.com>
2025-03-17 15:01:14 -0400

    rough css for signinPg

--- client/src/components/signInPg.jsx
+++ client/src/components/signInPg.jsx
@@ -2,15 +2,34 @@ import { Link } from "react-router-dom";
 
 const SignInPg = () => {
     return (
-        <div>
-            <h1 style={{ fontSize: '5rem' }}>Suikle</h1>
-            <div>
+        <div className="h-1/2 flex flex-col justify-evenly">
+            <h1 className="text-center" style={{ fontSize: "5rem" }}>
+                Suikle
+            </h1>
+            <section 
+            style={{backgroundColor:"#FBBB26"}}
+            className="h-2/3 flex flex-col justify-between items-center p-3 rounded">
+                <input
+                    className="p-3 rounded"
+                    style={{ fontSize: "18px" }}
+                    type="text"
+                    placeholder="E-mail Address"
+                />
+                <input
+                    className="p-3 rounded"
+                    style={{ fontSize: "18px" }}
+                    type="password"
+                    placeholder="Password"
+                />
                 <Link to="/home">
-                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
-                        SignIn
+                    <button
+                        style={{ backgroundColor: "#76D8FF" }}
+                        className="px-4 py-2 rounded"
+                    >
+                        Sign In
                     </button>
                 </Link>
-            </div>
+            </section>
         </div>
     );
 };
