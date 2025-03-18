bd24f3a97a002f210c53126ff12809484d5f341e
Eva Li <evali3101@gmail.com>
2025-03-18 00:04:42 -0400

    edited singingPg css responsivness

--- client/src/components/signInPg.jsx
+++ client/src/components/signInPg.jsx
@@ -2,29 +2,35 @@ import { Link } from "react-router-dom";
 
 const SignInPg = () => {
     return (
-        <div className="h-screen flex flex-col justify-center items-center">
-            <h1 className="text-center" style={{ fontSize: "5rem" }}>
+        <div className="min-h-screen flex flex-col justify-center items-center px-4 sm:px-6">
+            <h1
+                className="text-center sm:text-6xl md:text-7xl font-bold mb-8"
+                style={{ fontSize: "5rem" }}
+            >
                 Suikle
             </h1>
-            <section 
-            style={{backgroundColor:"#FBBB26"}}
-            className="h-1/3 flex flex-col justify-evenly items-center p-3 rounded">
+            <section
+                style={{ backgroundColor: "#FBBB26" }}
+                className="w-full max-w-md md:max-w-xl sm:w-1/3 sm:max-w-lg flex flex-col justify-evenly items-center p-6 sm:p-8 rounded-lg shadow-lg"
+            >
                 <input
-                    className="p-3 rounded"
+                    className="w-full p-3 rounded mb-4"
                     style={{ fontSize: "18px" }}
                     type="text"
                     placeholder="E-mail Address"
                 />
                 <input
-                    className="p-3 rounded"
+                    className="w-full p-3 rounded mb-4"
                     style={{ fontSize: "18px" }}
                     type="password"
                     placeholder="Password"
                 />
-                <Link to="/home">
+                <Link to="/home" className="w-1/3">
                     <button
-                        style={{ backgroundColor: "#76D8FF" }}
-                        className="px-4 py-2 rounded"
+                        style={{
+                            backgroundColor: "#76D8FF",
+                        }}
+                        className="w-full px-4 py-3 rounded font-semibold"
                     >
                         Sign In
                     </button>
