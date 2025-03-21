6ad7c79dd2044ba8825e3e5a326cd93d890f053e
AngelusQQ <william.ma9215@gmail.com>
2025-03-21 13:42:37 -0400

    Added word puzzle generation logic

--- controllers/word_puzzle.js
+++ controllers/word_puzzle.js
@@ -1,9 +1,76 @@
-const dictionary = require('../utils/dictionary')
-    .load_dictionary();
+const dictionary = require('../utils/dictionary');
+const words = dictionary.load();
+
+function generate_puzzle(num_of_letters = 7, count = 0) {
+    // if(num_of_letters < 4) {
+    //     console.log("minimum number of letters should be 4! Setting value to 4");
+    //     num_of_letters = 4;
+    // }
+
+    if(count >= 25) throw new Error('Possible Infinite Loop?');
+
+    const all_vowels = ["a", "e", "i", "o", "u"];
+    const all_consonants = ["b", "c", 'd', 'f', 'g', 'h', 'j', 'k', 
+    'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+
+    const num_of_vowels = Math.floor((Math.random() * 2) + 1);
+    const num_of_consonants = num_of_letters - num_of_vowels;
+    //Generating random vowels (1 or 2) and the remaining consonants
+    const puzzle = [];
+    for(let i = 0; i < num_of_vowels; i++) {
+        const vIndex = Math.floor(Math.random() * all_vowels.length);
+        puzzle.push(all_vowels[vIndex]);
+        all_vowels.splice(vIndex, 1);
+    }
+    for(let i = 0; i < num_of_consonants; i++) {
+        const cIndex = Math.floor(Math.random() * all_consonants.length);
+        puzzle.push(all_consonants[cIndex]);
+        all_consonants.splice(cIndex, 1);
+    }
+    //Randomizes the output of letters so the vowels aren't always in the first index.
+    for (let i = puzzle.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        [puzzle[i], puzzle[randomIndex]] = [puzzle[randomIndex], puzzle[i]];
+    }
+    const output = {
+        data: puzzle,
+        num_of_generations: count,
+        validity: get_validity(puzzle)
+    }
+    return output.validity.is_valid ? output : generate_puzzle(num_of_letters, (count+1));
+}
+
+function get_validity(letters) {
+    let valid_words = [];
+
+    for(let i = 0; i < words.length; i++) {
+        let add_word = true;
+        for(let j = 0; j < words[i].word.length; j++) {
+            if(!letters.includes(words[i].word[j])) {
+                add_word = false;
+                break;
+            }
+        }
+        if(add_word) {
+            valid_words.push(words[i]);
+        }
+    }
+
+    // valid_words = valid_words.sort((a,b) => a.count > b.count ? -1 : 1);
+    const true_words = valid_words
+        .filter(obj => obj.count >= dictionary.get_score_cutoff());    
+    
+    const output = {
+        is_valid: (true_words.length >= 15 ? true : false),
+        valid_words: valid_words,
+        length: true_words.length
+    }
+    return output;
+}
 
 module.exports = function(req, res) {
     try {
-        res.status(200).json({ data: dictionary[0] });
+        res.status(200).json(generate_puzzle(7));
     } catch(err) {
         console.error(err);
     }
--- utils/dictionary.js
+++ utils/dictionary.js
@@ -4,10 +4,15 @@ const path = require('path');
 const dictionary_path = path.join(__dirname, '../enable1.txt');
 if(!fs.existsSync(dictionary_path)) {
     throw new Error(`"enable1.txt" not found in ${dictionary_path}`);
-} 
+}
+
+const MAX_COUNT = 2000000;
 
 module.exports = {
-    load_dictionary: () => {
+    get_score_cutoff: () => {
+        return MAX_COUNT;
+    },
+    load: () => {
         const MAX_COUNT = 2000000;
         const dictionary = fs.readFileSync(dictionary_path, 'UTF-8')
             .split("\r\n")
