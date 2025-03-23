16ca132c2122e50293bac30650c4aadf78715d85
Eva Li <evali3101@gmail.com>
2025-03-22 21:46:38 -0400

    updated on siginPg css

--- client/src/components/signInPg.jsx
+++ client/src/components/signInPg.jsx
@@ -3,32 +3,22 @@ import { Link } from "react-router-dom";
 const SignInPg = () => {
     return (
         <div className="min-h-screen w-full flex flex-col justify-center items-center px-4 sm:px-6">
-            <h1 className="text-center text-5xl sm:text-6xl md:text-7xl font-bold mb-8">
+            <h1 className="text-center text-5xl sm:text-7xl md:text-6xl font-bold mb-8">
                 Suikle
             </h1>
-            <section
-                style={{ backgroundColor: "#FBBB26" }}
-                className="w-full max-w-md md:max-w-xl sm:w-1/3 sm:max-w-lg flex flex-col justify-evenly items-center p-6 sm:p-8 rounded-lg shadow-lg"
-            >
+            <section className="w-full sm:max-w-md md:max-w-xl sm:min-w-sm flex flex-col justify-evenly items-center p-6 sm:p-8 rounded-lg shadow-lg bg-[#FBBB26]">
                 <input
-                    className="w-full p-3 rounded mb-4"
-                    style={{ fontSize: "18px" }}
+                    className="w-full p-3 rounded mb-4 text-[18px] bg-white"
                     type="text"
                     placeholder="E-mail Address"
                 />
                 <input
-                    className="w-full p-3 rounded mb-4"
-                    style={{ fontSize: "18px" }}
+                    className="w-full p-3 rounded mb-4 text-[18px] bg-white"
                     type="password"
                     placeholder="Password"
                 />
                 <Link to="/home" className="w-1/3">
-                    <button
-                        style={{
-                            backgroundColor: "#76D8FF",
-                        }}
-                        className="w-full px-4 py-3 rounded font-semibold"
-                    >
+                    <button className="w-full px-4 py-3 rounded font-semibold bg-sky-500 hover:bg-sky-600 hover:shadow-lg active:scale-90 active:shadow-inner transition transform duration-200 cursor-pointer">
                         Sign In
                     </button>
                 </Link>
