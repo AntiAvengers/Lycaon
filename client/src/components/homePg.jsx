60ff617ffaaf87dc809ec9e719141ccef0e083a0
Eva Li <evali3101@gmail.com>
2025-03-26 14:36:59 -0400

    updated hompg css

--- client/src/components/homePg.jsx
+++ client/src/components/homePg.jsx
@@ -1,9 +1,9 @@
 const HomePg = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="flex w-full max-w-screen mx-8 sm:mt-8 md:mt-8 sm:my-0 my-8 border-1 border-gray-300 bg-[#E9ECEF] justify-center items-center">
+            <h1 className="text-5xl">Home</h1>
         </div>
-    )
+    );
 };
 
 export default HomePg;
