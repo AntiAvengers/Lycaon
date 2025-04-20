8f194d52884a7b302c9bf93ed2ec37975a3c89c9
Eva Li <evali3101@gmail.com>
2025-04-20 17:54:53 -0400

    added popup to pantryPg

--- client/src/components/pantryPg.jsx
+++ client/src/components/pantryPg.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const foods = [
     { src: "/assets/foods/apple.svg", label: "apple", value: 1, price: 5 },
     { src: "/assets/foods/meat-stick.svg", label: "meat", value: 2, price: 10 },
@@ -6,18 +8,35 @@ const foods = [
 ];
 
 const PantryPg = () => {
+    const [buy, setBuy] = useState(false);
+    const [selectedFood, setSelectedFood] = useState(null);
+
+    const closeBuy = () => {
+        setBuy(false);
+        setSelectedFood(null);
+    };
+
+    const handleBuy = (food) => {
+        setBuy(true);
+        setSelectedFood(food);
+    };
+
     return (
         <div className="w-[1000px] h-[550px] flex flex-col text-[#FFFFFF]">
             <h1 className="text-start text-[60px]">Pantry</h1>
             <div className="flex-1 flex items-center justify-center">
                 <ul className="w-full flex flex-row justify-between">
                     {foods.map((food) => (
-                        <li key={food.label} className="flex flex-col gap-2">
-                            <section className="w-[200px] h-[200px] bg-[#FFFFFF] rounded-[10px] shadow-md flex justify-center items-center">
+                        <li
+                            onClick={() => handleBuy(food)}
+                            key={food.label}
+                            className="group flex flex-col gap-2"
+                        >
+                            <section className="w-[200px] h-[200px] bg-[#FFFFFF] rounded-[10px] shadow-md flex justify-center items-center relative cursor-pointer  outline-[5px] outline-transparent transition-all duration-300 hover:outline-[#4A63E4] shadow-md active:inset-shadow-sm active:inset-shadow-[#000000] active:scale-95">
                                 <img
                                     src={food.src}
                                     alt={food.label}
-                                    className="w-[100px] h-[100px]"
+                                    className="w-[100px] h-[100px] transform transition duration-300 ease-in-out group-hover:-translate-y-1"
                                 />
                             </section>
                             <section className="flex flex-col">
@@ -28,6 +47,33 @@ const PantryPg = () => {
                     ))}
                 </ul>
             </div>
+
+            {buy && (
+                <div className="fixed inset-0 bg-[#140E28]/60 z-50 flex items-center justify-center">
+                    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#273472] rounded-[10px] shadow-lg z-50 w-[331px] h-[434px] flex flex-col items-center justify-evenly">
+                        <img
+                            src="/assets/icons/closeBtn.svg"
+                            alt="closeBtn"
+                            onClick={closeBuy}
+                            className="absolute top-[15px] right-[10px] cursor-pointer w-[40px] h-[40px]"
+                        />
+                        <h1 className="text-[30px] text-[#FCF4EF]">
+                            Buy {selectedFood.label}
+                        </h1>
+                        <img
+                            src={selectedFood.src}
+                            alt={selectedFood.label}
+                            className="w-[160px] h-[160px]"
+                        />
+                        <p className="text-[25px] text-[#FCF4EF]">
+                            {selectedFood.price} Shards
+                        </p>
+                        <button className="bg-[#FEFAF3] text-[#273472] rounded-[4px] px-[20px] py-[5px] text-[25px] shadow-[4px_4px_0_rgba(0,0,0,0.25)] active:translate-x-[4px] active:translate-y-[4px] active:shadow-none transition-all duration-75">
+                            Confirm Purchase
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
