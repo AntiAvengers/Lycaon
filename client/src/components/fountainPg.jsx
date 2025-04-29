e247126374e20acd50991b29c285500aad833885
Eva Li <evali3101@gmail.com>
2025-04-29 14:21:45 -0400

    added placeholder for fountain pulling

--- client/src/components/fountainPg.jsx
+++ client/src/components/fountainPg.jsx
@@ -10,13 +10,18 @@ const rates = [
 
 const FountainPg = () => {
     const [showRate, setShowRate] = useState(false);
+    const [pullMessage, setPullMessage] = useState("");
 
-    const closeRate = () => {
-        setShowRate(false);
-    };
+    //For Rate Btn
+    const closeRate = () => setShowRate(false);
+    const handleRate = () => setShowRate(true);
 
-    const handleRate = () => {
-        setShowRate(true);
+    //Popmessage for Pull Btns
+    const handlePull = (amount) => {
+        setPullMessage(
+            `You just pulled ${amount} page${amount > 1 ? "s" : ""}!`
+        );
+        setTimeout(() => setPullMessage(""), 2000);
     };
 
     useEffect(() => {
@@ -36,15 +41,25 @@ const FountainPg = () => {
                     />
                     <p className="text-[80px]">X pages</p>
                 </section>
+
+                {/* Pulling Btns */}
                 <section className="w-[65%] flex flex-row justify-between">
-                    <button className="w-[174px] h-[35px] rounded-[4px] text-[25px] text-center transition-all duration-75 px-[20px] bg-[#4A63E4] hover:bg-[#1D329F] rounded-[4px] shadow-[4px_4px_0_rgba(0,0,0,0.25)] active:bg-[#1D329F] active:translate-x-[4px] active:translate-y-[4px] active:shadow-none text-[25px] text-[#FFFFFF] cursor-pointer">
+                    <button
+                        onClick={() => handlePull(1)}
+                        className="w-[174px] h-[35px] rounded-[4px] text-[25px] text-center transition-all duration-75 px-[20px] bg-[#4A63E4] hover:bg-[#1D329F] rounded-[4px] shadow-[4px_4px_0_rgba(0,0,0,0.25)] active:bg-[#1D329F] active:translate-x-[4px] active:translate-y-[4px] active:shadow-none text-[25px] text-[#FFFFFF] cursor-pointer"
+                    >
                         1 Page
                     </button>
-                    <button className="w-[174px] h-[35px] rounded-[4px] text-[25px] text-center transition-all duration-75 px-[20px] bg-[#4A63E4] hover:bg-[#1D329F] rounded-[4px] shadow-[4px_4px_0_rgba(0,0,0,0.25)] active:bg-[#1D329F] active:translate-x-[4px] active:translate-y-[4px] active:shadow-none text-[25px] text-[#FFFFFF] cursor-pointer">
+                    <button
+                        onClick={() => handlePull(10)}
+                        className="w-[174px] h-[35px] rounded-[4px] text-[25px] text-center transition-all duration-75 px-[20px] bg-[#4A63E4] hover:bg-[#1D329F] rounded-[4px] shadow-[4px_4px_0_rgba(0,0,0,0.25)] active:bg-[#1D329F] active:translate-x-[4px] active:translate-y-[4px] active:shadow-none text-[25px] text-[#FFFFFF] cursor-pointer"
+                    >
                         10 Pages
                     </button>
                 </section>
             </div>
+
+            {/* Rate Detaile Popup */}
             <button
                 onClick={handleRate}
                 className="absolute right-[25px] bottom-[10px] underline text-[25px] cursor-pointer"
@@ -52,6 +67,7 @@ const FountainPg = () => {
                 Rate Details
             </button>
 
+            {/* Rate Popup  */}
             {showRate && (
                 <div className="w-[407px] h-[304px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inset-0 bg-[#FBBB26] z-50 rounded-[10px] p-[10px] flex flex-col items-center justify-between">
                     <img
@@ -73,6 +89,13 @@ const FountainPg = () => {
                             ))}
                         </div>
                     </section>
+                </div>
+            )}
+
+            {/* Pull Popup  */}
+            {pullMessage && (
+                <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 bg-[#FBBB26] text-black text-[30px] px-[20px] py-[10px] rounded-[10px] shadow-lg z-50">
+                    {pullMessage}
                 </div>
             )}
         </div>
