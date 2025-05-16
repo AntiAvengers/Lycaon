b16e2de0d9073ab5f0cac3ac6e6d03b182f09dda
AngelusQQ <william.ma9215@gmail.com>
2025-05-16 16:11:30 -0400

    Attempted fix for firebase connection

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -79,7 +79,7 @@ export function AuthProvider({ children }) {
     });
 
       return unsubscribe;
-    }, []);
+    }, [accessToken]);
 
   return (
     <AuthContext.Provider value={{ accessToken, setAccessToken, refreshAccessToken }}>
