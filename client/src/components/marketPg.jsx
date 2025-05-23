12713ef123e849a7d72ca002f76fd6ad8f48bc0e
Eva Li <evali3101@gmail.com>
2025-05-23 00:25:27 -0400

    updated market with bg

--- client/src/components/marketPg.jsx
+++ client/src/components/marketPg.jsx
@@ -14,9 +14,9 @@ const MarketPg = () => {
 
     return (
         <div className="w-full flex flex-col items-center justify-start gap-[15px]">
-            <section className="w-[1261px] h-[460px] bg-[#FFFFFF] leading-none px-[100px] pt-[100px]">
-                <h1 className="text-[80px]">Market</h1>
-                <p className="w-[495px] text-[25px]">
+            <section className="w-[1260px] h-[460px] bg-[url('/assets/bg/market-bg.png')] bg-center bg-no-repeat bg-contain px-[68px] text-[#FEFAF3] flex flex-col justify-center items-start">
+                <h1 className="text-[80px] leading-none">Market</h1>
+                <p className="w-[495px] text-[25px] leading-tight">
                     Step into the marketplace, where rare and radiant sprites
                     gathered by other adventurers shimmer with possibility.
                     Browse and trade to expand your collection with sprites.
